25bf9b370908f85f66de620655d9e190c341e35f
Hugo Tamayo <doctamayot@hotmail.com>
2018-11-01 18:13:11 -0500

    devise

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,7 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @expenses = Expense.order("date DESC")
+        @expenses = Expense.order("date DESC")
     if params[:concept].present?
       @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
     end
